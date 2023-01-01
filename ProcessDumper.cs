ef911245c5dd95e648dcc131890662de5f0c7198
come_ill_foo <rayveleaveme34@inbox.ru>
2023-01-01 20:00:31 +0000

    feat[apm]: process information dumper
    The class intention is to investigate how much information
    you're able to obtain using standard environment.

---
+++ ProcessDumper.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Diagnostics;
+
+namespace apm {
+    class ProcessDumper {
+        static int Main(string[] args) {
+            if (args.Length < 1) {
+                Console.WriteLine("expected process name");
+                return 22; // EINVAL
+            }
+            int pid = int.Parse(args[0]);
+            Process target = Process.GetProcessById(pid);
+
+            Console.WriteLine($"Process[{target.Id}:{target.BasePriority}]: {target.ProcessName}; HasExited: {target.HasExited}");
+            if (target.HasExited)
+                Console.WriteLine($"ExitCode: {target.ExitCode}; ExitTime: {target.ExitTime}");
+            Console.WriteLine($"MachineName: {target.MachineName}; MainModule: {target.MainModule}");
+            Console.WriteLine($"MainWindowTitle: {target.MainWindowTitle}");
+            Console.WriteLine($"MEM: PagedSystemMemorySize64: {target.PagedSystemMemorySize64}");
+            
+            return 0;
+        }
+    }
+}
